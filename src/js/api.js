0f4e773eb08ca8118945eb543938ba21eb9bbb16
huangyh <1049382543@qq.com>
2018-01-25 13:44:29 +0800

    modify api

--- src/js/api.js
+++ src/js/api.js
@@ -1,11 +1,11 @@
 import fetch from "./fetch.js";
 
 export default {
-    backendLogin: (data) => fetch("//huangyh.site/backend/login", "POST", data),
+    backendLogin: (data) => fetch("//localhost:3000/backend/login", "POST", data),
 
-    getPosts: (data) => fetch("//huangyh.site/api/v1/posts", "GET", data),
-    deletePost: (data) => fetch("//huangyh.site/api/v1/post/delete", "GET", data),
-    addPost: (data) => fetch("//huangyh.site/api/v1/post/add", "GET", data),
-    updatePost: (data) => fetch("//huangyh.site/api/v1/post/update", "POST", data),
-    backendSignout: (data) => fetch("//huangyh.site/api/v1/backend/signout", "GET", data)
+    getPosts: (data) => fetch("//localhost:3000/api/v1/posts", "GET", data),
+    deletePost: (data) => fetch("//localhost:3000/api/v1/post/delete", "GET", data),
+    addPost: (data) => fetch("//localhost:3000/api/v1/post/add", "GET", data),
+    updatePost: (data) => fetch("//localhost:3000/api/v1/post/update", "POST", data),
+    backendSignout: (data) => fetch("//localhost:3000/api/v1/backend/signout", "GET", data)
 }
