9736c0aa3724ee8e06412e99d36d34d654f88c6e
huangyh <1049382543@qq.com>
2018-01-25 14:13:27 +0800

    api modify

--- src/js/api.js
+++ src/js/api.js
@@ -1,11 +1,11 @@
 import fetch from "./fetch.js";
 
 export default {
-    backendLogin: (data) => fetch("//localhost:3000/backend/login", "POST", data),
+    backendLogin: (data) => fetch("/backend/login", "POST", data),
 
-    getPosts: (data) => fetch("//localhost:3000/api/v1/posts", "GET", data),
-    deletePost: (data) => fetch("//localhost:3000/api/v1/post/delete", "GET", data),
-    addPost: (data) => fetch("//localhost:3000/api/v1/post/add", "GET", data),
-    updatePost: (data) => fetch("//localhost:3000/api/v1/post/update", "POST", data),
-    backendSignout: (data) => fetch("//localhost:3000/api/v1/backend/signout", "GET", data)
+    getPosts: (data) => fetch("/api/v1/posts", "GET", data),
+    deletePost: (data) => fetch("/api/v1/post/delete", "GET", data),
+    addPost: (data) => fetch("/api/v1/post/add", "GET", data),
+    updatePost: (data) => fetch("/api/v1/post/update", "POST", data),
+    backendSignout: (data) => fetch("/api/v1/backend/signout", "GET", data)
 }
