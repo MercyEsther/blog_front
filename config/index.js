f611bbcc37cfb880bb8e2de15edf0681223dbedc
huangyh <1049382543@qq.com>
2018-01-25 10:12:15 +0800

    modify router

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../v1'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
@@ -51,7 +51,7 @@ module.exports = {
 
     productionSourceMap: true,
     // https://webpack.js.org/configuration/devtool/#production
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'cheap-module-inline-source-map',
 
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
